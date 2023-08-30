d0c65c9cbf9c1cd993f69c72d819122777d428aa
hurenn <suzukihuren070@gmail.com>
2023-08-30 23:06:53 +0900

    変数作成

--- Source/UQ5/Private/CPPHelloWorld.cpp
+++ Source/UQ5/Private/CPPHelloWorld.cpp
@@ -17,8 +17,10 @@ void ACPPHelloWorld::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	FString Message = "Hello New World!";
+
 	// PrintString関数を呼び出す
-	UKismetSystemLibrary::PrintString(this, "C++ Hello World!", true, true, FColor::Cyan, 2.f, TEXT("None"));
+	UKismetSystemLibrary::PrintString(this, Message, true, true, FColor::Cyan, 2.f, TEXT("None"));
 
 	// OutputLogに文字列を出力するマクロ
 	UE_LOG(LogTemp,Display, TEXT("Display Message"));
@@ -26,7 +28,7 @@ void ACPPHelloWorld::BeginPlay()
 	UE_LOG(LogTemp, Error, TEXT("Error Message"));
 
 	// Viewportの左上に文字列を出力する関数
-	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::White, "C++ Goodnight World", true, FVector2D(2.0f, 2.0f));
+	//GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::White, "C++ Goodnight World", true, FVector2D(2.0f, 2.0f));
 }
 
 // Called every frame
